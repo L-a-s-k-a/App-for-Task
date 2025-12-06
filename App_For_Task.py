4f854cc4973a282a17a09411bd1f4cba9945662b
L-a-s-k-a <nixonvi29@gmail.com>
2025-12-06 18:56:49 +0800

    The second change in the balance of variants

--- App_For_Task.py
+++ App_For_Task.py
@@ -7,7 +7,7 @@ def calculate_task():
     birth_date_str = entry.get().strip()
     
     # Если в поле только подсказка, выходим
-    if birth_date_str == "дд.мм.гггг" or not birth_date_str:
+    if birth_date_str == "ДД.ММ.ГГГГ" or not birth_date_str:
         messagebox.showwarning("Предупреждение", "Пожалуйста, введите дату рождения")
         clear_all()
         return
@@ -20,20 +20,19 @@ def calculate_task():
         # Вычисляем сумму дня и месяца
         task_calculate = birth.day + birth.month
         
-        # Вычисляем двоичное чилсо по дате рождения
+        # Вычисляем двоичное число по дате рождения
         bin_calculate = str(birth.day) + str(birth.month) + str(birth.year)
         bin_check = format(int(bin_calculate), '032b')
         
-        # Корректируем возраст, если день рождения еще не наступил в этом году
-        if (birth.year > today.year    or 
-            (birth.year == today.year and birth.month > today.month) or
-            (birth.year == today.year and birth.month == today.month and birth.day > today.day)):
+        # Обработка некорректно введённой даты рождения (Больше текущей даты)
+        if (birth.year > today.year or (birth.year == today.year and birth.month > today.month) or (birth.year == today.year and birth.month == today.month and birth.day > today.day)):
             error_date = True    
             messagebox.showwarning("Предупреждение", "Похоже, вы ещё не родились\n" "Пожалуйста, введите корректную дату рождения")
             task_calculate = 0
             bin_calculate = 0
             bin_check = 0
             clear_all()
+        # Обработка некорректно введённой даты рождения (Меньше текущей даты на 110 лет)
         elif (today.year - birth.year) > 110:
             error_date = True
             messagebox.showwarning("Предупреждение", "Похоже, вы умерли\n" "Пожалуйста, введите корректный год")
@@ -41,10 +40,8 @@ def calculate_task():
             bin_calculate = 0
             bin_check = 0
             clear_all()
-     
-        # truth_table = None
                     
-        # Определяем задание в зависимости от возраста
+        # Определяем задание в зависимости от даты рождения
         if 1 <= task_calculate <= 15:
             error_date = False   
             truth_table = "Запись числа в таблицу истинности происходит слева направо\n"
@@ -53,17 +50,17 @@ def calculate_task():
             else:
                 truth_table += "Все задания выполняются в базисе 4ИЛИ-НЕ\n"
             
-            if (int(task_calculate / 3) % 2) == 1:
+            if (int(task_calculate / 4) % 2) == 1:
                 truth_table += "Микросхема дешифратора К155ИД4\n"
                 truth_table += "Микросхема мультиплексора К155КП7"
-            elif (int(task_calculate / 2) % 2) == 0:
+            elif (int(task_calculate / 3) % 2) == 0:
                 truth_table += "Микросхема дешифратора К155ИД3\n"
                 truth_table += "Микросхема мультиплексора К155КП2"
-            elif (int(task_calculate / 2) % 2) == 1:
-                truth_table += "Микросхема дешифратора К155ИД10\n"
+            elif (int(task_calculate / 2) % 2) == 0:
+                truth_table += "Микросхема дешифратора К155ИД1\n"
                 truth_table += "Микросхема мультиплексора К155КП1"
             else:
-                truth_table += "Микросхема дешифратора К155ИД1\n"
+                truth_table += "Микросхема дешифратора К155ИД10\n"
                 truth_table += "Микросхема мультиплексора К155КП5"
         
         elif 16 <= task_calculate <= 25:
@@ -74,13 +71,13 @@ def calculate_task():
             else:
                 truth_table += "Все задания выполняются в базисе 3И-НЕ\n"
                 
-            if (int(task_calculate / 3) % 2) == 1:
-                truth_table += "Микросхема дешифратора К155ИД10\n"
-                truth_table += "Микросхема мультиплексора К155КП5"
-            elif (int(task_calculate / 2) % 2) == 0:
+            if (int(task_calculate / 4) % 2) == 1:
                 truth_table += "Микросхема дешифратора К155ИД1\n"
+                truth_table += "Микросхема мультиплексора К155КП5"
+            elif (int(task_calculate / 3) % 2) == 0:
+                truth_table += "Микросхема дешифратора К155ИД10\n"
                 truth_table += "Микросхема мультиплексора К155КП7"
-            elif (int(task_calculate / 2) % 2) == 1:
+            elif (int(task_calculate / 2) % 2) == 0:
                 truth_table += "Микросхема дешифратора К155ИД4\n"
                 truth_table += "Микросхема мультиплексора К155КП2"
             else:
@@ -95,14 +92,14 @@ def calculate_task():
             else:
                 truth_table += "Все задания выполняются в базисе 2И-НЕ\n"
                 
-            if (int(task_calculate / 3) % 2) == 1:
+            if (int(task_calculate / 4) % 2) == 1:
                 truth_table += "Микросхема дешифратора К155ИД3\n"
                 truth_table += "Микросхема мультиплексора К155КП5"
+            elif (int(task_calculate / 3) % 2) == 0:
+                truth_table += "Микросхема дешифратора К155ИД1\n"
+                truth_table += "Микросхема мультиплексора К155КП7"
             elif (int(task_calculate / 2) % 2) == 0:
                 truth_table += "Микросхема дешифратора К155ИД10\n"
-                truth_table += "Микросхема мультиплексора К155КП7"
-            elif (int(task_calculate / 2) % 2) == 1:
-                truth_table += "Микросхема дешифратора К155ИД1\n"
                 truth_table += "Микросхема мультиплексора К155КП2"
             else:
                 truth_table += "Микросхема дешифратора К155ИД4\n"
@@ -116,23 +113,22 @@ def calculate_task():
             else:
                 truth_table += "Все задания выполняются в базисе 3ИЛИ-НЕ\n"
             
-            if (int(task_calculate / 3) % 2) == 1:
-                truth_table += "Микросхема дешифратора К155ИД1\n"
+            if (int(task_calculate / 4) % 2) == 1:
+                truth_table += "Микросхема дешифратора К155ИД10\n"
                 truth_table += "Микросхема мультиплексора К155КП1"
-            elif (int(task_calculate / 2) % 2) == 0:
+            elif (int(task_calculate / 3) % 2) == 0:
                 truth_table += "Микросхема дешифратора К155ИД4\n"
                 truth_table += "Микросхема мультиплексора К155КП5"
-            elif (int(task_calculate / 2) % 2) == 1:
+            elif (int(task_calculate / 2) % 2) == 0:
                 truth_table += "Микросхема дешифратора К155ИД3\n"
                 truth_table += "Микросхема мультиплексора К155КП7"
             else:
-                truth_table += "Микросхема дешифратора К155ИД10\n"
+                truth_table += "Микросхема дешифратора К155ИД1\n"
                 truth_table += "Микросхема мультиплексора К155КП2"
         
         if error_date != True:    
             # Обновляем поле с результатом
             result_text_widget.config(state='normal', fg='black')
-            # result_text_widget.config(fg='black')  # Возвращаем обычный цвет текста
             result_text_widget.delete(1.0, tk.END)
             result_text_widget.insert(1.0, truth_table)
             result_text_widget.tag_add("center", "1.0", "end") # Применяем тег центрирования ко всему тексту
@@ -140,14 +136,10 @@ def calculate_task():
             
             # Обновляем поле с двоичным результатом
             binar_text_widget.config(state='normal', fg='black')
-            # binar_text_widget.config(fg='black')  # Возвращаем обычный цвет текста
             binar_text_widget.delete(1.0, tk.END)
             binar_text_widget.insert(1.0, (str(bin_calculate) + " = " + str(bin_check)))
             binar_text_widget.tag_add("center", "1.0", "end") # Применяем тег центрирования ко всему тексту
             binar_text_widget.config(state='disabled')
-        print(task_calculate % 2)
-        print(int(task_calculate / 3) % 2)
-        print(int(task_calculate / 2) % 2)
     
     except ValueError:
         messagebox.showerror("Ошибка", "Пожалуйста, введите дату в формате ДД.ММ.ГГГГ\nНапример: 12.08.2001")
@@ -204,9 +196,9 @@ try:
     # Устанавливаем его как иконку
     root.iconphoto(True, transparent_icon)
 except tk.TclError:
-    pass # Или можно оставить print
-root.geometry("655x460")  # Увеличили высоту для новой кнопки
-root.resizable(False, False)
+    pass
+root.geometry("655x460")  # Размеры окна
+root.resizable(False, False) # Запрет на изменение размеров по Х и У
 
 # Настраиваем стиль
 root.configure(bg='#f0f0f0')
